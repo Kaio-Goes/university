7c74d605c1c1c8a7b81432e65aa2a84ea72ee030
@kaio.goes <kaio.goes@spezi.com.br>
2024-07-24 00:53:11 -0300

    Feature page units

---
+++ lib/components/units_card.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class UnitsCard extends StatelessWidget {
+  final String local;
+  final String pole;
+  final String address;
+  final Function()? onPressed;
+  const UnitsCard({
+    super.key,
+    required this.local,
+    required this.pole,
+    required this.address,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3,
+      child: SizedBox(
+        width: 270,
+        child: Padding(
+          padding: const EdgeInsets.all(35.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                local,
+                style: TextStyle(fontSize: 16, color: Colors.red[600]),
+              ),
+              const SizedBox(height: 5),
+              Text(
+                pole,
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  const Icon(Icons.location_on, size: 16),
+                  const SizedBox(width: 5),
+                  Expanded(
+                    child: Text(
+                      address,
+                      style: const TextStyle(fontSize: 12),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 25),
+              Center(
+                child: ElevatedButton(
+                  onPressed: onPressed,
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                  child: const SizedBox(
+                    width: 160,
+                    child: Text(
+                      'Saiba Mais',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/units/units_page.dart
+++ lib/pages/units/units_page.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:university/components/app_bar_component.dart';
 import 'package:university/components/drawer_component.dart';
 import 'package:university/components/footer.dart';
+import 'package:university/components/units_card.dart';
 
-class UnitsPage extends StatelessWidget {
+class UnitsPage extends StatefulWidget {
   const UnitsPage({super.key});
+
+  @override
+  State<UnitsPage> createState() => _UnitsPageState();
+}
+
+class _UnitsPageState extends State<UnitsPage> {
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +20,86 @@ class UnitsPage extends StatelessWidget {
       appBar: appBarComponent(),
       drawer: const DrawerComponent(),
       body: SingleChildScrollView(
-        child: Column(
-          children: [Footer()],
-        ),
+        controller: _scrollController,
+        child: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          bool isSmallScreen = constraints.maxWidth < 600;
+          return Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: 180,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: [
+                          Color.fromARGB(255, 4, 19, 63),
+                          Color.fromARGB(255, 4, 19, 88),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 30,
+                    left: isSmallScreen
+                        ? 0
+                        : MediaQuery.of(context).size.height * 0.20,
+                    child: SizedBox(
+                      width: isSmallScreen ? 400 : 600,
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Início > Unidades',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 16),
+                            ),
+                            SizedBox(height: 10),
+                            Text('Nossas unidades',
+                                style: TextStyle(
+                                    fontSize: 35, color: Colors.white)),
+                            SizedBox(height: 10),
+                            Text(
+                              'Conheça mais sobre as nossas unidades.',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 226, 223, 223),
+                                fontSize: 13.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  const SizedBox(height: 45),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      UnitsCard(
+                        local: 'Planaltina / DF',
+                        pole: 'Polo Planaltina',
+                        address:
+                            'Conj. A, Lote 22, Residencial Sandray, Arapoangas',
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 180)
+                ],
+              ),
+              const Footer()
+            ],
+          );
+        }),
       ),
     );
   }

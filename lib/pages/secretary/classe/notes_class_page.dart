1d4de0a9ac0a6e59d2afad90d55553a483b3dee1
@kaio.goes <kaio.goes@spezi.com.br>
2025-03-27 09:24:25 -0300

    Feature history note in student class

--- lib/pages/secretary/classe/notes_class_page.dart
+++ lib/pages/secretary/classe/notes_class_page.dart
@@ -60,6 +60,10 @@ class _NotesClassPageState extends State<NotesClassPage> {
       setState(() {
         listSubject = subjects;
       });
+
+      for (var subject in subjects) {
+        await _loadNotes(subjectId: subject.uid, teacherId: subject.userId);
+      }
     } catch (e) {
       Exception("Erro loadgin subjects in class $e");
     }
@@ -93,27 +97,35 @@ class _NotesClassPageState extends State<NotesClassPage> {
 
       setState(() {
         listUserNote = userNotes;
-        print(listUserNote);
       });
     } catch (e) {
       Exception("Erro loaging User Notes $e");
     }
   }
 
-  _loadNotes({required subjectId}) async {
+  _loadNotes({required String subjectId, required String teacherId}) async {
     try {
       var notes = await NoteService().getListNotesByClass(
-        userId: AuthUserService().currentUser!.uid,
+        userId: teacherId,
         classId: widget.classFirebase.uid,
         subjectId: subjectId,
       );
 
       setState(() {
-        listNotes = notes;
+        listNotes.addAll(notes.where((newNote) =>
+            !listNotes.any((existingNote) => existingNote.uid == newNote.uid)));
       });
     } catch (e) {
       Exception('Erro ao carregar notas do usuário $e');
     }
+  }
+
+  double _calculateTotalScoreBySubject(String userId, String subjectId) {
+    return listUserNote
+        .where((userNote) =>
+            userNote.userId == userId && userNote.subjectId == subjectId)
+        .map((userNote) => double.tryParse(userNote.value) ?? 0.0)
+        .fold(0.0, (sum, value) => sum + value);
   }
 
   @override
@@ -159,82 +171,131 @@ class _NotesClassPageState extends State<NotesClassPage> {
                                       final user = listUser[index];
 
                                       return Card.outlined(
-                                        child: ListTile(
-                                          contentPadding:
-                                              const EdgeInsets.all(16),
-                                          hoverColor: Colors.grey[200],
-                                          title: Text(
-                                            user.name,
-                                            overflow: TextOverflow.ellipsis,
-                                            maxLines: 1,
-                                            style: const TextStyle(
-                                                fontSize: 16,
-                                                fontWeight: FontWeight.w800),
-                                          ),
-                                          subtitle: Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              const Text(
-                                                "Matérias:",
-                                                style: TextStyle(
-                                                    fontSize: 16,
-                                                    fontWeight:
-                                                        FontWeight.w700),
-                                              ),
-                                              const SizedBox(height: 5),
-                                              ...listSubject.map((subject) {
-                                                var teacher = listTeacher
-                                                    .where((t) =>
-                                                        t.uid == subject.userId)
-                                                    .firstOrNull;
-                                                return Column(
-                                                  crossAxisAlignment:
-                                                      CrossAxisAlignment.start,
-                                                  children: [
-                                                    const SizedBox(height: 5),
-                                                    Row(
+                                        child: SingleChildScrollView(
+                                          child: ListTile(
+                                            contentPadding:
+                                                const EdgeInsets.all(16),
+                                            hoverColor: Colors.grey[200],
+                                            title: Text(
+                                              user.name,
+                                              overflow: TextOverflow.ellipsis,
+                                              maxLines: 1,
+                                              style: const TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w800),
+                                            ),
+                                            subtitle: SingleChildScrollView(
+                                              scrollDirection: Axis.horizontal,
+                                              child: Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  const Text(
+                                                    "Matérias:",
+                                                    style: TextStyle(
+                                                        fontSize: 16,
+                                                        fontWeight:
+                                                            FontWeight.w700),
+                                                  ),
+                                                  const SizedBox(height: 5),
+                                                  ...listSubject.map((subject) {
+                                                    var teacher = listTeacher
+                                                        .where((t) =>
+                                                            t.uid ==
+                                                            subject.userId)
+                                                        .firstOrNull;
+                                                    return Column(
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .start,
                                                       children: [
-                                                        Text(
-                                                          subject.title,
-                                                          style:
-                                                              const TextStyle(
-                                                            fontSize: 15,
-                                                          ),
-                                                        ),
                                                         const SizedBox(
-                                                            width: 20),
-                                                        Wrap(
-                                                          spacing: 12.0,
-                                                          children: listUserNote
-                                                              .where((note) =>
-                                                                  note.userId ==
-                                                                      user
-                                                                          .uid &&
-                                                                  note.subjectId ==
-                                                                      subject
-                                                                          .uid)
-                                                              .map(
-                                                                (note) => Chip(
-                                                                  label: Text(note
-                                                                      .value
-                                                                      .toString()),
+                                                            height: 5),
+                                                        Row(
+                                                          children: [
+                                                            SizedBox(
+                                                              width: 180,
+                                                              child: Text(
+                                                                subject.title,
+                                                                style:
+                                                                    const TextStyle(
+                                                                  fontSize: 15,
                                                                 ),
-                                                              )
-                                                              .toList(),
+                                                                overflow:
+                                                                    TextOverflow
+                                                                        .ellipsis,
+                                                                maxLines: 1,
+                                                              ),
+                                                            ),
+                                                            const SizedBox(
+                                                                width: 20),
+                                                            Wrap(
+                                                              spacing: 12.0,
+                                                              children: listUserNote
+                                                                  .where((userNote) =>
+                                                                      userNote.userId ==
+                                                                          user
+                                                                              .uid &&
+                                                                      userNote.subjectId ==
+                                                                          subject
+                                                                              .uid)
+                                                                  .expand(
+                                                                      (userNote) {
+                                                                // Filtra todas as notas que correspondem ao userNote.noteId
+                                                                var matchingNotes =
+                                                                    listNotes
+                                                                        .where(
+                                                                  (n) =>
+                                                                      n.uid ==
+                                                                      userNote
+                                                                          .noteId,
+                                                                );
+
+                                                                // Mapeia cada nota correspondente para um Chip
+                                                                return matchingNotes
+                                                                    .map(
+                                                                  (note) =>
+                                                                      Chip(
+                                                                    label: Text(
+                                                                        "${note.title}: ${userNote.value.replaceAll(".", ",")}"),
+                                                                    backgroundColor:
+                                                                        const Color
+                                                                            .fromARGB(
+                                                                            24,
+                                                                            224,
+                                                                            248,
+                                                                            250),
+                                                                  ),
+                                                                );
+                                                              }).toList(),
+                                                            ),
+                                                            const SizedBox(
+                                                                width: 20),
+                                                            Text(
+                                                              "Soma: ${_calculateTotalScoreBySubject(user.uid, subject.uid).toStringAsFixed(2).replaceAll('.', ',')}",
+                                                              style: const TextStyle(
+                                                                  fontWeight:
+                                                                      FontWeight
+                                                                          .bold),
+                                                            ),
+                                                          ],
                                                         ),
+                                                        if (teacher != null)
+                                                          Text(
+                                                            "Professor ${teacher.name} ${teacher.surname}",
+                                                            style:
+                                                                const TextStyle(
+                                                                    fontSize:
+                                                                        12),
+                                                          ),
+                                                        const SizedBox(
+                                                            height: 5)
                                                       ],
-                                                    ),
-                                                    if (teacher != null)
-                                                      Text(
-                                                        "Professor ${teacher.name} ${teacher.surname}",
-                                                        style: const TextStyle(
-                                                            fontSize: 12),
-                                                      ),
-                                                  ],
-                                                );
-                                              }),
-                                            ],
+                                                    );
+                                                  }),
+                                                ],
+                                              ),
+                                            ),
                                           ),
                                         ),
                                       );

73e2f6d1bf6cd60b08e5eac9234acdb92202c464
@kaio.goes <kaio.goes@spezi.com.br>
2025-03-28 23:46:15 -0300

    feature gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,77 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:university/core/services/firebase_options_dev.dart';
 import 'package:university/pages/landingPage/dashboard/dashboard_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:university/pages/secretary/dashboard/dashboard_secretary_page.dart';
+import 'package:university/pages/student/dashboard/dashboard_student_page.dart';
+import 'package:university/pages/teacher/dashboard/dashboard_teacher_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: const FirebaseOptions(
-      apiKey: String.fromEnvironment('API_KEY'),
-      appId: String.fromEnvironment('APP_ID'),
-      messagingSenderId: String.fromEnvironment('MESSAGING_SENDER_ID'),
-      projectId: String.fromEnvironment('PROJECT_ID'),
-      authDomain: String.fromEnvironment('AUTH_DOMAIN'),
-      storageBucket: String.fromEnvironment('STORAGE_BUCKET'),
-      databaseURL: String.fromEnvironment('DATABASE_URL'),
+      apiKey: FirebaseOptionsDev.apiKey,
+      appId: FirebaseOptionsDev.appId,
+      messagingSenderId: FirebaseOptionsDev.messagingSenderId,
+      projectId: FirebaseOptionsDev.projectId,
+      authDomain: FirebaseOptionsDev.authDomain,
+      storageBucket: FirebaseOptionsDev.storageBucket,
+      databaseURL: FirebaseOptionsDev.databaseURL,
     ),
   );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  Future<Widget> _getInitialPage() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? role = prefs.getString('role');
+
+    if (role == 'admin') {
+      return const DashboardSecretaryPage();
+    } else if (role == 'teacher') {
+      return const DashboardTeacherPage();
+    } else if (role == 'student') {
+      return const DashboardStudentPage();
+    } else {
+      return const DashboardPage();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Anna Nery',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const DashboardPage(),
+    return FutureBuilder<Widget>(
+      future: _getInitialPage(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else {
+          return MaterialApp(
+            title: 'Anna Nery',
+            locale: const Locale('pt', 'BR'),
+            localizationsDelegates: const [
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [
+              Locale('en', 'US'),
+              Locale('pt', 'BR'),
+            ],
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: snapshot.data ?? const DashboardPage(),
+          );
+        }
+      },
     );
   }
 }

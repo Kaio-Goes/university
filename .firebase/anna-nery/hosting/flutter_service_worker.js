2b835bb8b6fc5106d503f8574623d0fd6d2b919b
@kaio.goes <kaio.goes@spezi.com.br>
2025-08-15 17:41:35 -0300

    fix

--- .firebase/anna-nery/hosting/flutter_service_worker.js
+++ .firebase/anna-nery/hosting/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"main.dart.js": "25bc4ee58c8aab5e9062b37c11ea9481",
+const RESOURCES = {"main.dart.js": "385af34427d343e4be07711d0c1c81fa",
 "index.html": "51d1e9f773770a50fe36c70f079ae9bb",
 "/": "51d1e9f773770a50fe36c70f079ae9bb",
 "canvaskit/skwasm.js": "ea559890a088fe28b4ddf70e17e60052",
@@ -19,7 +19,7 @@ const RESOURCES = {"main.dart.js": "25bc4ee58c8aab5e9062b37c11ea9481",
 "canvaskit/skwasm.js.symbols": "9fe690d47b904d72c7d020bd303adf16",
 "canvaskit/skwasm_st.js": "d1326ceef381ad382ab492ba5d96f04d",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
-"flutter_bootstrap.js": "5322335a335aa4001a3f489b29109194",
+"flutter_bootstrap.js": "acfed228ebf46d4b5b5fb696f30baadc",
 "favicon.png": "b24e74f5acc4798efde4ec5e8611b4d1",
 "version.json": "6362579264fcc5622c9362468ceac5d3",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
